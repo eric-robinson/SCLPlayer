f76253e0a50137a08ae27030005d1d8acc0e0b3d
Joe Rocca <Jprocca813@gmail.com>
2015-11-18 16:39:22 -0500

    iOS 9 Optimized

--- Pod/Classes/SCLPlayerViewController.m
+++ Pod/Classes/SCLPlayerViewController.m
@@ -163,7 +163,8 @@ NSString* const SCLPlayerPropertyBuying = @"buying";
     NSURL* scURL = [[NSBundle mainBundle] URLForResource:@"soundcloudPlayer" withExtension:@"html"];
     NSAssert(scURL, @"Unable to find soundcloudPlayer.html in source bundle");
     
-    NSString* urlParam = [[self.initialURL absoluteString] stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    NSString* urlParam = [[self.initialURL absoluteString] stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLHostAllowedCharacterSet]];
+    
     NSMutableString* configurationParams = [NSMutableString new];
     
     __block NSUInteger configIndex = 0;
@@ -188,7 +189,7 @@ NSString* const SCLPlayerPropertyBuying = @"buying";
                                                        withString:urlParam];
     
     htmlString = [htmlString stringByReplacingOccurrencesOfString:@"{{CONFIGURATION}}"
-                                                       withString:[configurationParams stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
+                                                       withString:[configurationParams stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLHostAllowedCharacterSet]]];
     
     
     [self.webview loadHTMLString:htmlString baseURL:nil];
